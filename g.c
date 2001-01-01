6159a803c5f600f9e0b0331f3cf3c61d6e925b5e
phreaker2600 <swwiley@gmail.com>
2001-01-01 01:06:42 +0000

    reads in circuit and initial state from input

--- g.c
+++ g.c
@@ -5,6 +5,8 @@
 #define OR 3
 #define XOR 4
 #define MAX_ITER 2
+#define MAX_ING 1
+#define MAX_INW 10
 
 typedef struct gate{
 	int a;
@@ -27,6 +29,15 @@ void status() {
 	}
 	printf("\n");
 }
+void pg(int n){
+	printf("gate %d a %d b %d t %d o %d\n",n,gates[n].a,gates[n].b,gates[n].t,gates[n].o);
+}
+void printlogic() {
+	int i=0;
+	for(i=0;i<W;i++){
+		pg(i);
+	}
+}
 void eg(int n) {
 	gate g=gates[n];
 	switch(g.t) {
@@ -46,16 +57,40 @@ void evaluate() {
 	}
 }
 void init() {
-	wires[0]=1;
+	wires[0]=0;
+	wires[1]=1;
 	gates[0].a=0;
 	gates[0].b=1;
 	gates[0].o=2;
-	gates[0].t=OR;
+	gates[0].t=NOT;
 	status();
+}
+void getgate(int n) {
+	gates[n].a=getchar()-'0';
+	gates[n].b=getchar()-'0';
+	gates[n].t=getchar()-'0';
+	gates[n].o=getchar()-'0';
+}
+void readin() {
+	int i=0;
+	for(i=0;i<MAX_ING;i++){
+		getgate(i);
+	}
+	for(i=0;i<MAX_INW;i++){
+		wires[i]=getchar()-'0';
+	}
+	printf("wires:");
+	for(i=0;i<W;i++){
+		printf(" %d",wires[i]);
+	}
+	printf("\n");
+	printlogic();
 }
 int main() {
 	int i=0;
-	init();
+//	init();
+	readin();
+	status();
 	for(i=0;i<MAX_ITER;i++){
 		evaluate();
 		status();
