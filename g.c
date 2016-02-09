71685e44052d8a0960ecb2819cadf0a613679e27
phreaker2600 <swwiley@gmail.com>
2016-02-09 17:29:48 +0000

    clean up

--- g.c
+++ g.c
@@ -5,25 +5,19 @@
 #define OR 3
 #define XOR 4
 #define MAX_ITER 2
-#define MAX_ING 1
 #define MAX_INW 10
 
 typedef struct gate{
 	int a;
 	int b;
-	int o;
 	int t;
+	int o;
 }gate;
 gate gates[W];
 int wires[W];
 int oldwires[W];
 void status() {
 	int i=0;
-	printf("STATUS:\n");
-	for(i=0;i<W;i++){
-		printf(" %d",i);
-	}
-	printf("\n");
 	for(i=0;i<W;i++){
 		printf(" %d",wires[i]);
 	}
@@ -31,12 +25,6 @@ void status() {
 }
 void pg(int n){
 	printf("gate %d a %d b %d t %d o %d\n",n,gates[n].a,gates[n].b,gates[n].t,gates[n].o);
-}
-void printlogic() {
-	int i=0;
-	for(i=0;i<W;i++){
-		pg(i);
-	}
 }
 void eg(int n) {
 	gate g=gates[n];
@@ -56,15 +44,6 @@ void evaluate() {
 		oldwires[i]=wires[i];
 	}
 }
-void init() {
-	wires[0]=0;
-	wires[1]=1;
-	gates[0].a=0;
-	gates[0].b=1;
-	gates[0].o=2;
-	gates[0].t=NOT;
-	status();
-}
 void getgate(int n) {
 	gates[n].a=getchar()-'0';
 	gates[n].b=getchar()-'0';
@@ -81,11 +60,10 @@ void readin() {
 		wires[i]=getchar()-'0';
 	}
 	printf("wires:");
+	status();
 	for(i=0;i<W;i++){
-		printf(" %d",wires[i]);
+		pg(i);
 	}
-	printf("\n");
-	printlogic();
 }
 int main() {
 	int i=0;
